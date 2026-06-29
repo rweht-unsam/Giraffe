b853334e48cd9513594383281f87de939695fadb
weht-unsam <rweht@unsam.edu.ar>
2026-06-29 17:50:33 +0000

    Corrijo mace_dyn.py

--- mace_dyn.py
+++ mace_dyn.py
@@ -16,21 +16,22 @@ from ase.optimize import BFGS, MDMin
 # ============================================
 from mace.calculators import mace_mp
 # ============================================
-macemp = mace_mp(model="./MACE-matpes-r2scan-omat-ft.model",default_dtype='float32')
+macemp = mace_mp(model="./MACE-matpes-r2scan-omat-ft.model",default_dtype='float32',device='cuda')
 calculator = macemp
 # ============================================d
 ahora0 = datetime.datetime.now()
 print(ahora0)
 # ============================================d
-atoms = read('More_Water_Inicial.extxyz')
+atoms = read('GB47_CCP_WAT.xyz')
+atoms.cell = [ 72.2   , 77.4   ,118.52  ]
 atoms.pbc=[ 1,1,1 ] 
 atoms.calc = macemp
 # ============================================
-fijos = np.array([], dtype=int)
-for atom in atoms:
-    if (atom.position[2] > 33.335 or atom.position[2] < 1.7850):
-        fijos = np.append(fijos,int(atom.index))
-atoms.set_constraint(FixAtoms(fijos))
+#fijos = np.array([], dtype=int)
+#for atom in atoms:
+#    if (atom.position[2] > 33.335 or atom.position[2] < 1.7850):
+#        fijos = np.append(fijos,int(atom.index))
+#atoms.set_constraint(FixAtoms(fijos))
 # ============================================d
 # Initialize velocities.
 T_init = 300  # Initial temperature in K
@@ -43,14 +44,14 @@ dyn = Langevin(atoms, 1.0 * units.fs, T_init * units.kB, 4.0)
 def wrap_atoms(a=atoms):
     a.wrap()
 dyn.attach(wrap_atoms,interval=10)
-trajectory_file = "More_Water.extxyz"
+trajectory_file = "GB47_WAT.extxyz"
 dyn.attach(lambda: write(trajectory_file, atoms, append=True), interval=100)
-dyn.attach(MDLogger(dyn, atoms, 'More_Water.log', header=False, stress=False,
+dyn.attach(MDLogger(dyn, atoms, 'GB47_WAT.log', header=False, stress=False,
            peratom=False, mode="a"), interval=100)
 # Correr la dinámica
 n_steps =  1000   # Number of steps to run
 dyn.run(n_steps)
-write('More_Water_Test.extxyz', atoms)
+write('GB47_WAT_final.extxyz', atoms)
 # Fin dinámica
 # ============================================d
 ahora1 = datetime.datetime.now()
